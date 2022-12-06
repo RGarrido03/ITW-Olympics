848371025438a2a7bd9f32d0d817e8e5240ab9dd
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2022-12-06 16:49:03 +0000

    Temporarily comment myScript.js (no use yet)

--- js/myScript.js
+++ js/myScript.js
@@ -1,4 +1,4 @@
-﻿$('document').ready(function () {
-    const carousel = new bootstrap.Carousel('#myCarousel', {
-        interval: 10000
-    });
+﻿// $('document').ready(function () {
+//     const carousel = new bootstrap.Carousel('#myCarousel', {
+//         interval: 10000
+//     });
