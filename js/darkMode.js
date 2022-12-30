6eae45eee0c82b950bf784f1f3b6ce4fa2371b64
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2022-12-30 15:40:38 +0000

    darkMode.js: Adicionar comentário com a fonte

--- js/darkMode.js
+++ js/darkMode.js
@@ -1,3 +1,7 @@
+// Retirado e adaptado de: https://getbootstrap.com/docs/5.3/customize/color-modes/#javascript
+// Foram removidas algumas linhas relativas a um menu dropdown para troca de cor.
+// Escolhemos apenas alterar o tema com base no sistema (usando media queries).
+
 const getPreferredTheme = function () {
     return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
 }
