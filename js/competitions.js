09324858c9d2c8e36bd9c7f021803d2920cd563b
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2022-12-30 02:05:03 +0000

    Competitions: Search filtra cards em vez de lista

--- js/competitions.js
+++ js/competitions.js
@@ -65,8 +65,8 @@ var vm = function () {
     self.order = ko.observable(0);
     self.count = ko.observable(1);
     self.setNewOrder = function () {
-        self.order($(event.target).val());
-        if (self.order() == 0) {
+        self.order($("#orderSelect option").filter(':selected').val());
+        if (self.order() == '0') {
             self.count(1);
         } else {
             self.count(self.totalPages());
@@ -75,7 +75,11 @@ var vm = function () {
         var composedUri = self.baseUri() + "?page=" + self.count() + "&pageSize=" + self.pagesize();
         ajaxHelper(composedUri, 'GET').done(function (data) {
             console.log(data);
-            self.records(data.Records.reverse());
+            if (self.order() == 0) {
+                self.records(data.Records);
+            } else {
+                self.records(data.Records.reverse());
+            }
             self.currentPage(data.CurrentPage);
             self.hasNext(data.HasNext);
             self.hasPrevious(data.HasPrevious);
@@ -127,39 +131,18 @@ var vm = function () {
                 $.get("http://192.168.160.58/Olympics/api/Competitions/SearchByName", {
                     q: searchQuery
                 }, function (data) {
-                    $("#AutocompleteList").html("");
-                    var list = "";
-
-                    if (data.length > 0) {
-                        if (data.length > 4) {
-                            var count = 4;
-                            var more = true;
-                        } else {
-                            var count = data.length;
-                            var more = false;
-                        }
-
-                        data.slice(0, count).map(function (item) {
-                            list += '<a href="competitionsDetails.html?id=' + item.Id + '" class="list-group-item list-group-item-action">' + item.Name.replace(new RegExp('(' + searchQuery + ')', 'ig'), '<span class="fw-semibold">$1</span>') + '</a>';
-                        });
-
-                        if (more) {
-                            list += '<a class="list-group-item list-group-item-action">' + (data.length - 4) + ' outros resultados</a>';
-                        }
-
-                        $("#AutocompleteList").html(list);
-
+                    console.log(data);
+                    if (self.order() == 0) {
+                        self.records(data);
                     } else {
-                        $("#AutocompleteList").html(
-                            '<a class="list-group-item list-group-item-action">No results found</a>'
-                        );
+                        self.records(data.reverse());
                     }
-                    $("#AutocompleteList").fadeIn("fast");
                 });
-            }, 500);
+            }, 1000);
         }
         else {
-            $("#AutocompleteList").fadeOut("fast");
+            clearTimeout(typingTimeout);
+            self.setNewOrder();
         }
     };
 
@@ -174,7 +157,7 @@ var vm = function () {
             $("#scrollToTop").slideDown('fast');
         }
         
-        if ($(window).scrollTop() + $(window).height() > $(document).height() - 425) {
+        if (($(window).scrollTop() + $(window).height() > $(document).height() - 425) && $("#searchInput").val().length == 0) {
             count = self.fetchMoreData();
         }
         return true;
