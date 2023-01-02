7ceb1b799e3bb947b3f17bf3c493bc2651e217f9
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2023-01-02 02:36:07 +0000

    gamesDetails: Fix API bug

--- js/gamesDetails.js
+++ js/gamesDetails.js
@@ -62,7 +62,7 @@ var vm = function () {
     self.getMoreData = async function () {
         console.log("Getting full details of game with ID " + self.Id());
         showLoading();
-        var composedUri = self.baseUri() + "/FullDetails?id=" + id;
+        var composedUri = self.baseUri() + "FullDetails?id=" + id;
         await ajaxHelper(composedUri, 'GET').done(function (data) {
             console.log(data);
             self.Summary(false);
