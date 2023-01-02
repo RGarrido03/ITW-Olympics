5f8cf23fab346b5e28f69f659bfacc9d6e39079a
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2023-01-02 02:45:57 +0000

    athleteDetails: redesign

--- js/athletesDetails.js
+++ js/athletesDetails.js
@@ -4,7 +4,7 @@ var vm = function () {
     //---Variáveis locais
     var self = this;
     self.baseUri = ko.observable('http://192.168.160.58/Olympics/api/Athletes/');
-    self.displayName = 'Olympic Athletes edition Details';
+    self.displayName = 'Olympic Athlete Details';
     self.error = ko.observable('');
     self.passingMessage = ko.observable('');
     //--- Data Record
@@ -15,17 +15,55 @@ var vm = function () {
     self.Weight = ko.observable('');
     self.BornDate = ko.observable('');
     self.BornPlace = ko.observableArray('');
+    self.Birth = ko.computed({
+        read: function () {
+            if (self.BornDate() == null || self.BornDate() == '' && self.BornPlace() == null || self.BornPlace() == '') {
+                return "NA";
+            } else if (self.BornDate() == null || self.BornDate() == '') {
+                return self.BornPlace();
+            } else if (self.BornPlace() == null || self.BornPlace() == '') {
+                return self.BornDate();
+            } else {
+                return self.BornDate() + ", " + self.BornPlace();
+            }
+        },
+        write: function (value, value_b) {
+            self.BornDate(value);
+            self.BornPlace(value_b);
+        }
+    });
     self.DiedDate = ko.observable('');
     self.DiedPlace = ko.observable('');
+    self.Death = ko.computed({
+        read: function () {
+            if (self.DiedDate() == null || self.DiedDate() == '' && self.DiedPlace() == null || self.DiedPlace() == '') {
+                return "NA";
+            } else if (self.DiedDate() == null || self.DiedDate() == '') {
+                return self.DiedPlace();
+            } else if (self.DiedPlace() == null || self.DiedPlace() == '') {
+                return self.DiedDate();
+            } else {
+                return self.DiedDate() + ", " + self.DiedPlace();
+            }
+        },
+        write: function (value, value_b) {
+            self.DiedDate(value);
+            self.DiedPlace(value_b);
+        }
+    });
     self.Photo = ko.observable('');
     self.OlympediaLink = ko.observable('');
-
+    self.Summary = ko.observable(true);
+    self.Games = ko.observableArray([]);
+    self.Modalities = ko.observableArray([]);
+    self.Competitions = ko.observableArray([]);
+    self.Medals = ko.observableArray([]);
 
     //--- Page Events
-    self.activate = function (id) {
-        console.log('CALL: getAthletes...');
+    self.activate = async function (id) {
+        console.log('Athelete ID: ' + id);
         var composedUri = self.baseUri() + id;
-        ajaxHelper(composedUri, 'GET').done(function (data) {
+        await ajaxHelper(composedUri, 'GET').done(function (data) {
             console.log(data);
             hideLoading();
             self.Id(data.Id);
@@ -33,14 +71,49 @@ var vm = function () {
             self.Sex(data.Sex);
             self.Height(data.Height);
             self.Weight(data.Weight);
-            self.BornDate(data.BornDate);
+            self.BornDate(data.BornDate ? new Date(data.BornDate).toLocaleDateString("pt-PT") : null);
             self.BornPlace(data.BornPlace);
-            self.DiedDate(data.DiedDate);
+            self.DiedDate(data.DiedDate ? new Date(data.DiedDate).toLocaleDateString("pt-PT"): null);
             self.DiedPlace(data.DiedPlace);
             self.OlympediaLink(data.OlympediaLink);
             self.Photo(data.Photo);
         });
     };
+
+    self.getMoreData = async function () {
+        console.log("Getting full details of game with ID " + self.Id());
+        showLoading();
+        var composedUri = self.baseUri() + "/FullDetails?id=" + id;
+        await ajaxHelper(composedUri, 'GET').done(function (data) {
+            console.log(data);
+            self.Summary(false);
+            self.Games(data.Games);
+            self.Modalities(data.Modalities);
+            self.Competitions(data.Competitions);
+            self.Medals(data.Medals);
+        }).then(hideLoading());
+    };
+
+    self.scrollToTop = function () {
+        $('html, body').animate({ scrollTop: 0 }, 'fast');
+    };
+
+    self.goBack = function () {
+        if (window.history.length > 1) {
+            history.back();
+        } else {
+            window.location.href = '/athletes.html';
+        }
+    };
+
+    $(window).on("resize scroll", function () {
+        if ($(window).scrollTop() == 0) {
+            $("#scrollToTop").slideUp('fast');
+        } else {
+            $("#scrollToTop").slideDown('fast');
+        }
+        return true;
+    });
 
     //--- Internal functions
     function ajaxHelper(uri, method, data) {
@@ -50,11 +123,13 @@ var vm = function () {
             url: uri,
             dataType: 'json',
             contentType: 'application/json',
-            data: data ? JSON.stringify(data) : null,
+            data: data ? data : null,
             error: function (jqXHR, textStatus, errorThrown) {
                 console.log("AJAX Call[" + uri + "] Fail...");
                 hideLoading();
                 self.error(errorThrown);
+                const toast = new bootstrap.Toast($('#errorToast'));
+                toast.show();
             }
         });
     }
@@ -88,13 +163,8 @@ var vm = function () {
 
     //--- start ....
     showLoading();
-    var pg = getUrlParameter('id');
-    console.log(pg);
-    if (pg == undefined)
-        self.activate(1);
-    else {
-        self.activate(pg);
-    }
+    var id = getUrlParameter('id');
+    self.activate(id);
     console.log("VM initialized!");
 };
 
