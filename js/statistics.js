b72023b2189d3e004f483e5a1b8d2a3903d8ebd9
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2023-01-03 10:22:30 +0000

    Statistics

--- js/statistics.js
+++ js/statistics.js
@@ -3,18 +3,181 @@ var vm = function () {
     console.log('ViewModel initiated...');
     //---Variáveis locais
     var self = this;
-    self.baseUri = ko.observable('http://192.168.160.58/Olympics/api/countries/');
-    self.displayName = 'Olympic Countries edition Details';
+    self.baseUri = ko.observable('http://192.168.160.58/Olympics/api/Statistics/');
+    self.displayName = 'Olympic Games statistics';
     self.error = ko.observable('');
     self.passingMessage = ko.observable('');
+    self.Games = ko.observableArray([]);
+    self.Athletes = ko.observableArray([]);
+    self.Competitions = ko.observableArray([]);
+    self.Countries = ko.observableArray([]);
+    self.Modalities = ko.observableArray([]);
+    self.CountriesList = ko.observableArray([]);
+    self.MedalsGold = ko.observableArray([]);
+    self.MedalsSilver = ko.observableArray([]);
+    self.MedalsBronze = ko.observableArray([]);
 
-    self.createGraph = function () {
-        const ctx = document.getElementById('MedalsPerCountryChart');
+    self.fetchData = async function () {
+        console.log('CALL: fetchData...');
+        console.log('Calling ajaxHelper for Athletes...');
+        ajaxHelper(self.baseUri() + 'Games_Athletes', 'GET').done(function (data) {
+            console.log(data);
+            data.forEach(function (item) {
+                self.Athletes.push(item.Counter);
+                self.Games.push(item.Name);
+            });
+            console.log('Games: ' + self.Games());
+            console.log('Athletes: ' + self.Athletes());
+            self.createAthletesGraph();
+        });
+
+        console.log('Calling ajaxHelper for Competitions...');
+        ajaxHelper(self.baseUri() + 'Games_Competitions', 'GET').done(function (data) {
+            console.log(data);
+            data.forEach(function (item) {
+                self.Competitions.push(item.Counter);
+            });
+            console.log('Competitions: ' + self.Competitions());
+            self.createCompetitionsGraph();
+        });
+
+        console.log('Calling ajaxHelper for Countries...');
+        ajaxHelper(self.baseUri() + 'Games_Countries', 'GET').done(function (data) {
+            console.log(data);
+            data.forEach(function (item) {
+                self.Countries.push(item.Counter);
+            });
+            console.log('Countries: ' + self.Countries());
+            self.createCountriesGraph();
+        });
+
+        console.log('Calling ajaxHelper for Modalities...');
+        ajaxHelper(self.baseUri() + 'Games_Modalities', 'GET').done(function (data) {
+            console.log(data);
+            data.forEach(function (item) {
+                self.Modalities.push(item.Counter);
+            });
+            console.log('Modalities: ' + self.Modalities());
+            self.createModalitiesGraph();
+        });
+
+        console.log('Calling ajaxHelper for Medals...');
+        ajaxHelper(self.baseUri() + 'Medals_Country', 'GET').done(function (data) {
+            console.log(data);
+            data.forEach(function (item) {
+                self.CountriesList.push(item.CountryName);
+                self.MedalsGold.push(item.Medals[0].Counter);
+                self.MedalsSilver.push(item.Medals[1].Counter);
+                self.MedalsBronze.push(item.Medals[2].Counter);
+            });
+            console.log('Gold Medals: ' + self.MedalsGold());
+            console.log('Silver Medals: ' + self.MedalsSilver());
+            console.log('Bronze Medals: ' + self.MedalsBronze());
+            self.createMedalsGraph();
+            hideLoading();
+        });
+
+    };
+
+    self.createAthletesGraph = function () {
+        const ctx = document.getElementById('AthletesChart');
 
         new Chart(ctx, {
             type: 'bar',
             data: {
-                labels: self.MedalsGames(),
+                labels: self.Games(),
+                datasets: [{
+                    label: 'Athletes',
+                    data: self.Athletes(),
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+    }
+
+    self.createCompetitionsGraph = function () {
+        const ctx = document.getElementById('CompetitionsChart');
+
+        new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: self.Games(),
+                datasets: [{
+                    label: 'Competitions',
+                    data: self.Competitions(),
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+    }
+
+    self.createCountriesGraph = function () {
+        const ctx = document.getElementById('CountriesChart');
+
+        new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: self.Games(),
+                datasets: [{
+                    label: 'Countries',
+                    data: self.Countries(),
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+    }
+
+    self.createModalitiesGraph = function () {
+        const ctx = document.getElementById('ModalitiesChart');
+
+        new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: self.Games(),
+                datasets: [{
+                    label: 'Modalities',
+                    data: self.Modalities(),
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+    }
+
+    self.createMedalsGraph = function () {
+        const ctx = document.getElementById('MedalsChart');
+
+        new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: self.CountriesList(),
                 datasets: [{
                     label: 'Gold',
                     data: self.MedalsGold(),
@@ -35,7 +198,7 @@ var vm = function () {
                 scales: {
                     y: {
                         beginAtZero: true,
-                        stacked: true,
+                        stacked: true
                     },
                     x: {
                         stacked: true
@@ -91,6 +254,7 @@ var vm = function () {
 
     //--- start ....
     showLoading();
+    self.fetchData();
 };
 
 $(document).ready(function () {
