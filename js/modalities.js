cda560fe3cb271cd8a66413d7e38702d9bd2dd5a
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2023-01-04 14:52:43 +0000

    modalities: favorites

--- js/modalities.js
+++ js/modalities.js
@@ -19,6 +19,44 @@ var vm = function () {
     self.count = ko.observable(1);
     self.hasMore = ko.observable(true);
     self.searchLoading = ko.observable(false);
+    self.Favorites = {
+        "athletes": [],
+        "countries": [],
+        "competitions": [],
+        "games": [],
+        "modalities": []
+    }
+    self.FavoritesArray = ko.observableArray([]);
+
+    //--- Page Events
+    self.getFavorites = function () {
+        if (localStorage.getItem("Favorites")) {
+            self.Favorites = JSON.parse(localStorage.getItem("Favorites"));
+        } else {
+            localStorage.setItem("Favorites", JSON.stringify(self.Favorites));
+        }
+        console.log("Current favorites: ", self.Favorites);
+
+        self.FavoritesArray(self.Favorites.modalities);
+        self.FavoritesArray().forEach(function (id) {
+            $("#fav" + id).addClass("text-danger").removeClass("text-body");
+        });
+    };
+
+    self.setFavorites = function (id) {
+        console.log(id)
+        var idx = self.FavoritesArray.indexOf(id.toString());
+        if (idx == -1) {
+            $("#fav" + id).addClass("text-danger").removeClass("text-body-secondary");
+            self.FavoritesArray.push(String(id))
+        } else {
+            $("#fav" + id).removeClass("text-danger").addClass("text-body-secondary");
+            self.FavoritesArray.splice(idx, 1);
+        };
+        console.log(self.FavoritesArray());
+        console.log(self.Favorites);
+        localStorage.setItem('Favorites', JSON.stringify(self.Favorites))
+    };
 
     //--- Page Events
     self.fetchData = async function (isNew) {
@@ -82,8 +120,8 @@ var vm = function () {
             self.pagesize(data.PageSize);
             self.totalPages(data.TotalPages);
             self.totalRecords(data.TotalRecords);
+            self.getFavorites();
             hideLoading();
-            //self.SetFavourites();
             loading = false;
         });
 
@@ -110,6 +148,7 @@ var vm = function () {
                     } else {
                         self.records(data.reverse());
                     }
+                    self.getFavorites();
                     self.searchLoading(false);
                 });
             }, 1000);
